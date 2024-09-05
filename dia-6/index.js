5b9d896b78013426d274b38ab6f99f3d04e7fcf1
Heithor Barbosa <heithor.barbosa321@gmail.com>
2024-09-05 11:02:06 -0300

    dia-6 ex3

--- dia-6/index.js
+++ dia-6/index.js
@@ -1,3 +1,3 @@
-for (let digito = 0; digito <= 50; digito += 5) {
-  console.log(" de 5 em 5 até 50: " + digito);
+for (let digito = 50; digito >= 0; digito -= 5) {
+  console.log(" de 5 em 5 até 0: " + digito);
 }
