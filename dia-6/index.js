99e6f977d8167644a904a598b103dca15d0a3713
Heithor Barbosa <heithor.barbosa321@gmail.com>
2024-09-25 23:31:00 -0300

    aula 10 guanabara operadores

--- dia-6/index.js
+++ dia-6/index.js
@@ -1,10 +1,10 @@
-function somar() {
-  let numero1 = document.getElementById("n1");
-  let numero2 = document.getElementById("n2");
-  let res = document.getElementById("res");
-  let n1 = Number(numero1.value);
-  let n2 = Number(numero2.value);
-  let soma = n1 + n2;
-  res.innerHTML = `o valor é ${soma}`;
+function multa() {
+  let vel = document.getElementById("vel1");
+  let multass = document.getElementById("multas");
+  let velo = Number(vel.value);
+  if (velo > 60) {
+    multass.innerText = "multado";
+  } else {
+    multas.innerText = "pode ir";
+  }
 }
-console.log("olá");
